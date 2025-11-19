e451915721b8c92303fd62288077bbddd39cc2d6
florentlalise <florent.lalise@gmail.com>
2025-11-19 22:59:22 +0100

    change browser database structure

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,6 +1,5 @@
 const DB_NAME = "emistla-db";
 const DB_VERSION = 2;
-const WALLPAPER_STORE = "wallpaper";
 const DATA_STORE = "data";
 const WALLPAPER_KEY = "custom-wallpaper";
 const NOTEPAD_KEY = "notepad-content";
@@ -18,12 +17,6 @@ function openDatabase(): Promise<IDBDatabase> {
     request.onupgradeneeded = (event) => {
       const db = (event.target as IDBOpenDBRequest).result;
 
-      // Create wallpaper store if it doesn't exist
-      if (!db.objectStoreNames.contains(WALLPAPER_STORE)) {
-        db.createObjectStore(WALLPAPER_STORE);
-      }
-
-      // Create data store for text content and other data
       if (!db.objectStoreNames.contains(DATA_STORE)) {
         db.createObjectStore(DATA_STORE);
       }
@@ -40,8 +33,8 @@ export async function saveWallpaper(file: File): Promise<string> {
   const db = await openDatabase();
 
   return new Promise((resolve, reject) => {
-    const transaction = db.transaction([WALLPAPER_STORE], "readwrite");
-    const store = transaction.objectStore(WALLPAPER_STORE);
+    const transaction = db.transaction([DATA_STORE], "readwrite");
+    const store = transaction.objectStore(DATA_STORE);
 
     const request = store.put(file, WALLPAPER_KEY);
 
@@ -63,8 +56,8 @@ export async function loadWallpaper(): Promise<string | null> {
     const db = await openDatabase();
 
     return new Promise((resolve, reject) => {
-      const transaction = db.transaction([WALLPAPER_STORE], "readonly");
-      const store = transaction.objectStore(WALLPAPER_STORE);
+      const transaction = db.transaction([DATA_STORE], "readonly");
+      const store = transaction.objectStore(DATA_STORE);
       const request = store.get(WALLPAPER_KEY);
 
       request.onsuccess = () => {
@@ -92,8 +85,8 @@ export async function deleteWallpaper(): Promise<void> {
   const db = await openDatabase();
 
   return new Promise((resolve, reject) => {
-    const transaction = db.transaction([WALLPAPER_STORE], "readwrite");
-    const store = transaction.objectStore(WALLPAPER_STORE);
+    const transaction = db.transaction([DATA_STORE], "readwrite");
+    const store = transaction.objectStore(DATA_STORE);
     const request = store.delete(WALLPAPER_KEY);
 
     request.onsuccess = () => resolve();
